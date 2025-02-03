76702bb949629bec2f606ed221a515822af0b486
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-02-03 04:51:58 +0300

    no message

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version: 5.5
+
+import PackageDescription
+
+let package = Package(name: "NativeAppGlue", products: [
+    .library(name: "NativeAppGlue", targets: ["NativeAppGlue"]),
+], targets: [
+    .target(name: "NativeAppGlue"),
+])
