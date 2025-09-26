ec24abfba90ae6196c55a92a5eb42f360fe62ced
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-09-27 01:37:32 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,5 @@ import PackageDescription
 let package = Package(name: "NativeAppGlue", products: [
     .library(name: "NativeAppGlue", targets: ["NativeAppGlue"]),
 ], targets: [
-    .target(name: "NativeAppGlue", linkerSettings: [
-        .linkedLibrary("Android"),
-    ]),
+    .target(name: "NativeAppGlue"),
 ])
